442221a70c7c410cee1cab09b27ca878371ad04c
onyoo <roberto9m9@gmail.com>
2016-01-16 15:55:56 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+category_1 = Category.create!(name: "Ruby Posts")
+category_2 = Category.create!(name: "Learning Korean")
+
+boss = Author.create!(name: "The Boss", bio: "I was born. Then I became the boss.", genre: "comedy")
+jim = Author.create!(name: "Jim", bio: "I was born. Then I died.", genre: "other")
+
+post_1 = Post.create!(title: "Seeding Databases! Ten Mind-blowing Facts!", description: "Lose your mind", author_id: boss.id)
+post_2 = Post.create!(title: "한국어를 잘 하시는 방법", description: "많이 해~", author_id: jim.id, category_id: category_2.id)
