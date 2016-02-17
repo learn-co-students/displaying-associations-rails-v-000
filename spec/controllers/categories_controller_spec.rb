51e7c8ab26655bc0d6080d0502b315da5b7aa488
Joe Burgess <joe@flatironschool.com>
2016-02-17 04:56:10 -0500

    Addes id to show category test Without the id it always fails! Fixes issue #3

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe CategoriesController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      category = Category.create(name: "New Category")
+      get :show, id: category
       expect(response).to have_http_status(:success)
     end
   end
