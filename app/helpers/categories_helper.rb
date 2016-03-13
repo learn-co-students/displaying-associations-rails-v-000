18b6102d79bbd449d66c435684dcde01ad7bfc29
azheng249 <andy.zheng249@gmail.com>
2016-03-13 13:37:06 -0400

    Done.

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,13 @@
 module CategoriesHelper
+  def names
+    Category.all.pluck(:name)
+  end
+
+  def recent_category_posts(category)
+    category.posts.order(updated_at: :desc)
+  end
+
+  def authors(category)
+    category.posts.include(:authors)
+  end
 end
