5bc48de8edc63ba3b7b9865c37661b4e29dd38dc
Tyler Smith <oats000@hotmail.com>
2016-04-23 09:57:18 -0400

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 describe 'navigate' do
   before do
     @post = Post.create(title: "My Post", description: "My post desc")
+    clickbait = Category.create(name: "Motivation")
+    @post.category = clickbait
+    @post.save
   end
 
   it 'shows the title on the show page in a h1 tag' do
@@ -25,6 +28,9 @@ describe 'form' do
 
   it 'shows an update form that submits content and redirects and prints out params' do
     @post = Post.create(title: "My Post", description: "My post desc")
+    clickbait = Category.create(name: "Motivation")
+    @post.category = clickbait
+    @post.save
 
     visit edit_post_path(@post)
 
