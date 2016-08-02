0b30fc2eb2cc96c5eecab9cee4c2ed4ef3725986
wook kim <wooke2e@gmail.com>
2016-08-02 21:24:17 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'categories/show'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-  resources :categories
+  resources :categories, only: [:index, :show, :new, :create, :edit, :update]
 end
