b10eaa5930e5d49452779353f99c497870d75e55
akizzles <aykim002@gmail.com>
2016-08-08 19:30:06 -0400

    Done.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,15 @@
 class CategoriesController < ApplicationController
+
   def show
     @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.create(params[:name])
+    redirect_to category_path(@category)
   end
 end
