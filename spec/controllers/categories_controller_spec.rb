655e7da3318a25e074e6575bd241fe829bf54ad0
gabe <gj+code@mail.co.de>
2016-08-27 16:34:23 -0400

    add test for categories/edit route; use @category instance variable

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
   describe "GET #show" do
-    it "returns http success" do
-      category = Category.create(name: "New Category")
-      get :show, id: category
+    it "returns HTTP success" do
+      get :show, id: @category
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns HTTP success" do
+      get :edit, id: @category
       expect(response).to have_http_status(:success)
     end
   end
