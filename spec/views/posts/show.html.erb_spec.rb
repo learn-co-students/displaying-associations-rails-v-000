1b46a77e26029edb0f18378d34de0a9c170dbb5c
gabe <gj+code@mail.co.de>
2016-08-27 20:25:01 -0400

    add test for link to post's category

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -1,13 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "posts/show", type: :feature do
-  it 'shows the title on the show page in an <h1> tag' do
+  it "shows the post's title in an <h1> tag" do
     visit post_path(@post)
     expect(page).to have_css("h1", text: "My Post")
   end
 
-  it 'shows the description on the show page in a <p> tag' do
+  it "shows the post's description in a <p> tag" do
     visit post_path(@post)
     expect(page).to have_css("p", text: "My post desc")
+  end
+
+  it "contains a link to the post's category" do
+    visit post_path(@post)
+    expect(page).to have_css("a", text: @post.category.name)
   end
 end
