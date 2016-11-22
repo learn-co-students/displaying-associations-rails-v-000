db0a2dfb2cc3dd7b328f71788ecacc3b75991dd9
kkterai <kkterai@gmail.com>
2016-11-22 11:56:52 -0800

    Done.

--- db/migrate/20151230232412_create_posts.rb
+++ db/migrate/20151230232412_create_posts.rb
@@ -3,7 +3,10 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :description
-
+      t.boolean :post_status
+      t.integer :author_id
+      t.integer :category_id
+      
       t.timestamps null: false
     end
   end
