45ad30d4bf1aa282e2ba25808f0493dc2f1351ea
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-10 17:01:41 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 20160113163910) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "post_status"
     t.integer  "author_id"
     t.integer  "category_id"
