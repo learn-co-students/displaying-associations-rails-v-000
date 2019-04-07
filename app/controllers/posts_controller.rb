ca4a283f203cf50666c40d57c5aa8aa118afdd1f
Katie Moosman <moosman.katie@gmail.com>
2019-04-07 06:12:13 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,8 +13,8 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		post = Post.create(params[:post])
-		redirect_to post_path(post)
+		@post = Post.create(params[:post])
+		redirect_to post_path(@post)
 	end
 
 	def edit
