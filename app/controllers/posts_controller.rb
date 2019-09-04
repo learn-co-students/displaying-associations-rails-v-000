14c98c2ff20a869c01ccb67d579aab38e5d10862
esugano <esugano.git@gmail.com>
2019-09-03 18:46:59 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,8 +23,8 @@ class PostsController < ApplicationController
 	end
 
 	def update
-		post = Post.find(params[:id])
-		post.update(params.require(:post))
-		redirect_to post_path(post)
+		@post = Post.find(params[:id])
+		@post.update(params.require(:post).permit(:title, :description, :post_status))
+		redirect_to post_path(@post)
 	end
 end
