dea5039378c8e7beaf3f1ed3d8290bec33eb1bf3
dianawhalen <whalendianaj@gmail.com>
2019-09-08 17:40:22 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,8 +13,8 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		post = Post.create(params[:post])
-		redirect_to post_path(post)
+		@post = Post.create(params[:post])
+		redirect_to post_path(@post)
 	end
 
 	def edit
@@ -23,8 +23,8 @@ class PostsController < ApplicationController
 	end
 
 	def update
-		post = Post.find(params[:id])
-		post.update(params.require(:post))
-		redirect_to post_path(post)
+		@post = Post.find(params[:id])
+		@post.update(params.require(:post).permit(:title, :description, :post_status))
+		redirect_to post_path(@post)
 	end
 end
