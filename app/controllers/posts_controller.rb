bd09a129daf39932884a1180c0c3a7676980a767
Blake A. Long <blake.a.long@gmail.com>
2020-03-05 12:46:39 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
+	before_action :find_id, only: [:show, :edit, :update]
 	def index
 		@posts = Post.all
 	end
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	def new
@@ -13,22 +13,24 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		post = Post.create(post_params)
-		redirect_to post_path(post)
+		@post = Post.create(post_params)
+		redirect_to post_path(@post)
 	end
 
 	def edit
-		@post = Post.find(params[:id])
 		@categories = Category.all
 	end
 
 	def update
-	  post = Post.find(params[:id])
-	  post.update(post_params)
-	  redirect_to post_path(post)
+	  @post.update(post_params)
+	  redirect_to post_path(@post)
 	end
 
   private
+
+  def find_id
+	  @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :description)
