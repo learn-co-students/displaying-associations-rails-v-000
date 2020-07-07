3ddfab95cb9dd1068bec1e23f946dbb2f4f81058
klovae <elyse.klova@gmail.com>
2020-07-07 16:01:50 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+clickbait = Category.create!(name: "Motivation")
+clickbait.posts.create!(title: "10 Things Successful People Tell Themselves Every Morning")
+clickbait.posts.create!(title: "Entrepreneurs Swear By This Product")
+clickbait.posts.create!(title: "Learn This Meditation Technique And Develop Superpowers")
+
+movies = Category.create!(name: "Movies")
+movies.posts.create!(title: "But Does The Dog Die?")
+movies.posts.create!(title: "2000s Actions Classics No One Remembers")
